efd9adefd0527a744a069603c104015c87411499
Cholo0813 <hanschristian.lawas@cvsu.edu.ph>
2025-01-12 16:21:11 +0800

    Implement create student functionality

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudentModule } from './student/student.module';
+import { StudentModule } from './student/student.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root', // Replace with your DB username
+      password: '', // Replace with your DB password
+      database: 'db_student', // Your database name
+      entities: [Student],
+      synchronize: true, // This will auto-create the database schema
+    }),
+    StudentModule,
+  ],
+})
+export class AppModule {}
